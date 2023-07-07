cb76f6dfb36b9222028fb3cc5726ff42c5bd030c
Akshay Kulkarni <84171172+akshaykulkarni02@users.noreply.github.com>
2023-07-07 12:59:13 +0530

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -3,8 +3,12 @@ package com.akshay.databse;
 public class Entity {
 	public static void main(String[] args) {
 		Student student = new Student();
+
 		student.createDatabase();
 		student.createTable();
 		student.createData();
+		student.updateData();
+		student.readData();
+
 	}
 }
